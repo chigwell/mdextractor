d0eda2735c271d3c8b8bf8570d93226f2b80b47d
E. Evstafiev <ee345@cam.ac.uk>
2024-08-28 09:23:43 +0100

    docs: update README.md

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='mdextractor',
-    version='0.0.3',
+    version='0.0.4',
     author='Eugene Evstafev',
     author_email='chigwel@gmail.com',
     description='Extract Markdown code blocks from text strings.',
